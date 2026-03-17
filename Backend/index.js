d4765725e1bd53be3fd66914341839857aaa209e
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-17 22:10:58 +0530

    project

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const connectDB = require('./src/config/db');
 connectDB();
 
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log("Server is running on Port 3000");
